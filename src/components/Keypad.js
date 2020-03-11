d20b2f862ef44b8cdac87052b0ceab1a475e7501
Ivan <ivan1501@gmail.com>
2020-03-11 13:11:48 +0100

    Change in style. Layout of calculator changed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -6,8 +6,8 @@ class Keypad extends Component {
         return (
             <div className="button">
                 <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
-                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
                 <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
+                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
                 <button name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br/>
 
 
